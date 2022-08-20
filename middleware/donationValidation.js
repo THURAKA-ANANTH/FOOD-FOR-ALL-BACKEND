c6d6cd88ef93fff2e0edaec13b5e98aeba5a6bfe
Akila Kavinda <akilakavinda909@gmail.com>
2022-08-20 21:52:34 +0530

    donation CRUD

--- middleware/donationValidation.js
+++ middleware/donationValidation.js
@@ -3,7 +3,15 @@ const { body, validationResult } = require("express-validator");
 exports.validate = (method) => {
   switch (method) {
     case "createDonation": {
-      return [body("test", "test not exist").exists()];
+      return [
+        body("donationTitle", "Title does not exist").exists(),
+        body("contactNumber", "Contact number does not exist or invalid")
+          .exists()
+          .isInt()
+          .isLength({ min: 10, max: 10 }),
+        body("email", "Email does not exist or invalid").exists().isEmail(),
+        body("donationDescription", "Description does not exist").exists(),
+      ];
     }
   }
 };
