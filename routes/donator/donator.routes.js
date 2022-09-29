422933ea70f7c21e340980bd885187b0e039a759
Akila Kavinda <akilakavinda909@gmail.com>
2022-09-29 21:39:11 +0530

    send request

--- routes/donator/donator.routes.js
+++ routes/donator/donator.routes.js
@@ -10,6 +10,9 @@ const {
   getRejectedDonations,
 } = require("../../controllers/donator/getDonations");
 const getOneDonationDetails = require("../../controllers/donator/getOneDonation");
+const {
+  sendDonationRequest,
+} = require("../../controllers/donator/sendDonationRequest");
 const { testBase64 } = require("../../controllers/donator/test");
 const { validate } = require("../../middleware/donationValidation");
 
@@ -18,12 +21,13 @@ const router = express.Router();
 router.post("/createDonation", validate("createDonation"), createDonation);
 router.get("/getDonations", getAllDonations);
 router.delete("/deleteDonation/:id", deleteDonation);
-router.get("/getCompletedDonations", getCompletedDonations);
-router.get("/getOngoingDonations", getOngoingDonations);
-router.get("/getPendingDonations", getPendingDonations);
-router.get("/getRejectedDonations", getRejectedDonations);
+router.get("/getCompletedDonations/;id", getCompletedDonations);
+router.get("/getOngoingDonations/:id", getOngoingDonations);
+router.get("/getPendingDonations/:id", getPendingDonations);
+router.get("/getRejectedDonations/:id", getRejectedDonations);
 router.get("/getOneDonation/:id", getOneDonationDetails);
 router.put("/updateDonation/:id", editDonation);
+router.post("/sendRequest", sendDonationRequest);
 
 router.post("/test", testBase64);
 
