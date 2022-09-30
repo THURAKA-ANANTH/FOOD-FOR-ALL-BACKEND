fad7064621c93a5cc804f7ebda350f5862c6aa5b
Yasith Chandula <76646376+yasithchandula@users.noreply.github.com>
2022-09-30 08:33:00 +0530

    auth_update

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,27 +5,59 @@ const jwt = require('jsonwebtoken');
 
 const handleLogin = async (req, res) => {
     console.log("PASSEDDD")
-    console.log(req.body)
+    // console.log(req.body)
     const { username, password, email } = req.body;
+    let foundUser;
     if ((!username||!email)&&!password) return res.status(400).json({ 'message': 'Username and password are required.' });
 
-    const foundUser = await User.findOne({ username: username }).exec();
-    console.log(foundUser)
-    const foundUser1= await Organization.findOne({email:email}).exec();
-    console.log(foundUser1)
-    if (!foundUser||!foundUser1) return res.sendStatus(401); //Unauthorized 
+    foundUser = await User.findOne({ username: username }).exec();
+    if (!foundUser){
+        foundUser = await Organization.findOne({email: username }).exec();
+    } else if (!foundUser) {
+        return res.sendStatus(401); //Unauthorized 
+    }
+    
+    
+    // const foundUser = await User.findOne({ username: email }).exec();
+    // if(username){
+    //     console.log(username)
+    //     foundUser = await User.findOne({ username: username }).exec();
+    //     console.log("username",foundUser)
+        
+        
+
+    // } else if (email){
+    //     foundUser = await Organization.findOne({username: email }).exec();
+    //     console.log("email",foundUser)
+        
+        
+    // }
+    // const foundUser1 = await User.findOne({ username: username }).exec();
+    // console.log(foundUser)
+    // const foundUser= await Organization.findOne({email:username}).exec();
+    // console.log(foundUser1)
+    
     // evaluate password 
-    const match = await bcrypt.compare(password, foundUser.password);
-    const match1=await bcrypt.compare(password,foundUser1.password);
+     const match = await bcrypt.compare(password, foundUser.password);
+    //const match = (password==foundUser.password);
+    const match1=await bcrypt.compare(password,foundUser.password);
+    console.log(foundUser)
+    console.log(password)
+    console.log(foundUser.password)
+    console.log(match);
+
     if (match) {
-        const roles = Object.values(foundUser.roles).filter(Boolean);
+        const roles = foundUser.roles;
+        const _id=(foundUser._id);
+        
         console.log("PASSED1");
         // create JWTs
         const accessToken = jwt.sign(
             {
                 "UserInfo": {
                     "username": foundUser.username,
-                    "roles": roles
+                    "roles": roles,
+                    "_id":_id
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
@@ -41,45 +73,48 @@ const handleLogin = async (req, res) => {
         const result = await foundUser.save();
         console.log(result);
         console.log(roles);
+        console.log(_id)
 
         // Creates Secure Cookie with refresh token
         res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
         // Send authorization roles and access token to user
-        res.json({ roles, accessToken });
+        res.json({ roles, accessToken,_id });
+        console.log(accessToken);
+        }
+    // } else if(match1) {
+    //     console.log("PASSED2");
+    //     const roles = Object.values(foundUser.roles).filter(Boolean);
+    //     // create JWTs
+    //     const accessToken = jwt.sign(
+    //         {
+    //             "UserInfo": {
+    //                 "username": foundUser.email,
+    //                 "roles": roles
+    //             }
+    //         },
+    //         process.env.ACCESS_TOKEN_SECRET,
+    //         { expiresIn: '1h' }
+    //     );
+    //     const refreshToken = jwt.sign(
+    //         { "username": foundUser1.email },
+    //         process.env.REFRESH_TOKEN_SECRET,
+    //         { expiresIn: '1d' }
+    //     );
+    //     // Saving refreshToken with current user
+    //     foundUser1.refreshToken = refreshToken;
+    //     const result = await foundUser1.save();
+    //     console.log(result);
+    //     console.log(roles);
 
-    } else if(match1) {
-        console.log("PASSED2");
-        const roles = Object.values(foundUser1.roles).filter(Boolean);
-        // create JWTs
-        const accessToken = jwt.sign(
-            {
-                "UserInfo": {
-                    "username": foundUser1.username,
-                    "roles": roles
-                }
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '1h' }
-        );
-        const refreshToken = jwt.sign(
-            { "username": foundUser1.username },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: '1d' }
-        );
-        // Saving refreshToken with current user
-        foundUser1.refreshToken = refreshToken;
-        const result = await foundUser1.save();
-        console.log(result);
-        console.log(roles);
+    //     // Creates Secure Cookie with refresh token
+    //     res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
-        // Creates Secure Cookie with refresh token
-        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
-
-        // Send authorization roles and access token to user
-        res.json({ roles, accessToken });
+    //     // Send authorization roles and access token to user
+    //     res.json({ roles, accessToken });
         
-    }else{
+    // }
+        else{
         res.sendStatus(401);
     }
 }
