0f042ae48f3605f43423c408e9318ee57048a801
Thushal Shaminda <it20216146@my.sliit.lk>
2022-10-11 22:07:51 +0530

    request crud operations added

---
+++ controllers/request/viewRequest.js
@@ -0,0 +1,25 @@
+const Request = require("../../models/requestFund.model");
+
+const getRequestersRequest = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        await Request.find({ userId: userId })
+        .then((requests) => {
+            res.status(200).send({
+                requests
+            });
+        }).catch((err) => {
+            res.status(500).send({
+                msg: "Error with fetching data",
+                error: err,
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    getRequestersRequest,
+}
