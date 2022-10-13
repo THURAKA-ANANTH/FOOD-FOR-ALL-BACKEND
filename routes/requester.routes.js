58a689229cd36ca3561ea5508e1ae126e4e4756f
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-10-14 01:27:52 +0530

    view all requests

--- routes/requester.routes.js
+++ routes/requester.routes.js
@@ -4,6 +4,7 @@ const { createRequest } = require("../controllers/requester/CreateRequest");
 const { requesterSignUp } = require("../controllers/requester/RequesterSignup");
 const { viewUserProfile } = require("../controllers/requester/ViewProfile");
 const { updateProfile } = require("../controllers/requester/UpdateProfile");
+const { getAllRequests } = require("../controllers/request/allRequests");
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.post("/requesterSignUp", requesterSignUp);
 router.post("/createRequest", createRequest);
 router.post("/viewProfile", viewUserProfile);
 router.post("/updateProfile", updateProfile);
+
+router.get("/allrequests", getAllRequests);
 
 module.exports = router;
