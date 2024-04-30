49a14e3cb79263e4de7bebf16bd3fb67c0090d5d
THURAKA-ANANTH <116439107+THURAKA-ANANTH@users.noreply.github.com>
2024-05-01 00:00:56 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:8070',
     'http://localhost:3500',
     'http://localhost:3000',
+    'https://food-for-all-hq2d.vercel.app',
     'http://localhost:8070/'
 ];
 
